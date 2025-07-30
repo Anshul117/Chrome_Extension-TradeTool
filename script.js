aac4b797938a99795caf1e534c5bbb7150df4e1b
Anshul117 <anshulverma1973@gmail.com>
2025-07-30 17:52:00 +0530

    grab

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ let input = document.getElementById("input-el")
 let save = document.getElementById("input-btn")
 let list = document.getElementById("ul-el")
 let del = document.getElementById("delete-btn")
+let saveTab = document.getElementById("tab-btn")
 const myLeadFromStorage = JSON.parse(localStorage.getItem("myLead"))
+
 if(myLeadFromStorage){
     myLead = myLeadFromStorage
     render(myLead)
-}
+}//// In Manifest V3, Chrome separates permissions (what APIs your extension can access) and host_permissions
+
+saveTab.addEventListener("click" ,()=>{  //
+    chrome.tabs.query({active:true , currentWindow:true}, function(tab){
+        let url = tab[0].url
+        myLead.push(url)
+        localStorage.setItem("myLead" , JSON.stringify(myLead))
+        render(myLead) 
+    })
+})
+
 function render(leads){
     let li = ""
    for(let i = 0 ; i<leads.length ; i++){
