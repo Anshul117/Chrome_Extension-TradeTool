281dce85fe65681e2f309824ba24af5d851edb50
Anshul117 <anshulverma1973@gmail.com>
2025-07-30 16:39:07 +0530

    ref

--- script.js
+++ script.js
@@ -2,21 +2,33 @@ let myLead = []
 let input = document.getElementById("input-el")
 let save = document.getElementById("input-btn")
 let list = document.getElementById("ul-el")
-
-
-save.addEventListener("click" , ()=>{
-    myLead.push(input.value)
-    renderList()
-    input.value = ""
-})
-
-function renderList(){
+let del = document.getElementById("delete-btn")
+const myLeadFromStorage = JSON.parse(localStorage.getItem("myLead"))
+if(myLeadFromStorage){
+    myLead = myLeadFromStorage
+    render(myLead)
+}
+function render(leads){
     let li = ""
-   for(let i = 0 ; i<myLead.length ; i++){
+   for(let i = 0 ; i<leads.length ; i++){
         li+= ` <li>  
-                    <a target = '_blank' href = "${myLead[i]}"> ${myLead[i]}</a> 
+                    <a target = '_blank' href = "${leads[i]}"> ${leads[i]}</a> 
                 </li>`
    }
    list.innerHTML=li
 
 }
+del.addEventListener("dblclick" , ()=>{
+    myLead = []
+    localStorage.clear()
+    render(myLead)
+})
+
+
+save.addEventListener("click" , ()=>{
+    myLead.push(input.value)
+    localStorage.setItem("myLead" , JSON.stringify(myLead))
+    render(myLead)
+    input.value = ""
+})
+
